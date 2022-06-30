4046922eef6d1cc34c11d29429e1223696911f84
И.В.Нелюб <neluyb.v@mail.ru>
2022-06-30 16:20:32 +0300

    New version

---
+++ Lesson-9-Lists2.py
@@ -0,0 +1,11 @@
+cars =["Volvo", "Renault", "Audi", "Skoda", "Hyndai"]
+
+for i in cars:
+    print(i.upper())
+
+for i in range(1, 6):
+    print(i)
+
+number_list = list(range(0, 10))
+print(number_list)
+
