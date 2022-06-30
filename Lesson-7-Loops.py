2e59215b2b49885befa13bec9bc5cea7ab59326c
ivan <neluyb.v@mail.ru>
2022-06-30 13:38:09 +0300

    install Message

---
+++ Lesson-7-Loops.py
@@ -0,0 +1,14 @@
+for x in range(0,100 + 1, 2):
+    print("Number x = " + str(x))
+    print(x)
+    if x == 50:
+        break
+
+print("******")
+print("------")
+
+while True:
+    print(x)
+    x = x + 1
+    if x == 100:
+        break
---
+++ Lesson-8-Lists.py
@@ -0,0 +1,11 @@
+cities = ['New York', 'Moskow', 'Amsterdam', 'Minsk', 'new deli']
+print(cities)
+print(len(cities))
+
+print(cities[0])
+print(cities[-1])
+print(cities[4].title())
+print(cities[3].upper())
+
+cities[1] = 'Warshaw'
+print(cities)
