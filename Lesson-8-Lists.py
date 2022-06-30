684bd19dd8003ac3f975fd75f46e895336530d02
И.В.Нелюб <neluyb.v@mail.ru>
2022-06-30 16:11:39 +0300

    New version

--- Lesson-8-Lists.py
+++ Lesson-8-Lists.py
@@ -1,11 +1,38 @@
-cities = ['New York', 'Moskow', 'Amsterdam', 'Minsk', 'new deli']
-print(cities)
-print(len(cities))
+cities = ['New York', 'Moskow', 'Amsterdam', 'Minsk', 'new deli']   #Creat list 'cites'
+print(cities)   #Write list 'cites'
+print(len(cities)) #Write length list
 
-print(cities[0])
+print(cities[0]) #write
 print(cities[-1])
 print(cities[4].title())
 print(cities[3].upper())
+cities[4] = cities[4].title()
 
 cities[1] = 'Warshaw'
 print(cities)
+
+cities.append("Riga")
+cities.append("Tallin")
+print(cities)
+
+#Insert
+cities.insert(4, "Berlin")
+print(cities)
+
+#Remove nunber
+del cities[-3]
+print(cities)
+
+#Remove
+cities.remove("New York")
+print(cities)
+
+#Sort
+deleted_city = cities.pop()
+print("Deleted city is: " + deleted_city)
+print(cities)
+cities.sort()
+print(cities)
+cities.reverse()
+print(cities)
+
